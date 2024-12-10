2add56b158fa449b4b127a7245d8e5c420163af7
RamanKawani <RamanKhalid888@gmail.com>
2024-12-10 12:59:50 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,13 @@
-from app import db
+from extensions import db
 
 class Resource(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    type = db.Column(db.String(50), nullable=False)  # Shelter, healthcare, etc.
-    address = db.Column(db.String(200), nullable=False)
-    phone = db.Column(db.String(15), nullable=True)
-    hours = db.Column(db.String(50), nullable=True)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
-    description = db.Column(db.Text, nullable=True)
+    description = db.Column(db.String(255), nullable=False)
 
-    def __repr__(self):
-        return f'<Resource {self.name}>'
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
