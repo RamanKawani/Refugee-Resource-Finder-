c99ae90d068b4dd7b45c4477c0d8a58c4603f4d2
RamanKawani <RamanKhalid888@gmail.com>
2024-12-10 14:32:34 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,17 @@
-# flask_app.py
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+    @app.route('/')
+    def home():
+        return "Hello, World!"
+
+    return app
 
 def start_flask_app():
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0", port=5000)  # Use 0.0.0.0 to allow external access
+
+if __name__ == "__main__":
+    start_flask_app()
