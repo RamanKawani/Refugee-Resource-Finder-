c8000f8da11322a18097bcdff8c337fd8fab3908
RamanKawani <RamanKhalid888@gmail.com>
2024-12-10 13:33:47 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,12 +1,11 @@
 from flask import Blueprint, jsonify
 
-bp = Blueprint('main', __name__)
+bp = Blueprint('views', __name__)
 
-# Example route that returns a list of resources
-@bp.route('/resources', methods=['GET'])
-def get_resources():
-    resources = [
-        {"name": "Resource 1", "description": "Description of Resource 1"},
-        {"name": "Resource 2", "description": "Description of Resource 2"}
-    ]
-    return jsonify(resources)
+@bp.route('/')
+def home():
+    return jsonify({"message": "Welcome to the Flask API!"})
+
+@bp.route('/hello')
+def hello():
+    return jsonify({"message": "Hello, Flask!"})
