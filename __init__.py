5b59b79876414d9ff5feeffa2290a9c4d6d4e7b3
RamanKawani <RamanKhalid888@gmail.com>
2024-12-10 13:33:18 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # Register routes (views)
+    # Import routes
     from . import views
     app.register_blueprint(views.bp)
 
